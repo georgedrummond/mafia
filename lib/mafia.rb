34f801c8df4b287f950aa8e2d9a609bd0af096e3
George Drummond <me@georgedrummond.com>
2011-10-10 14:13:36 +0200

    create git repo and templates location

--- lib/mafia.rb
+++ lib/mafia.rb
@@ -8,45 +8,51 @@ module Mafia
     argument :name
     
     def self.source_root
-      File.dirname(__FILE__)
+      File.expand_path(File.join(File.dirname(__FILE__), 'mafia/templates'))
     end
     
     def create_gemspec_file
-      template("mafia/templates/gemspec.tt", "#{name}/#{name}.gemspec")
+      template("gemspec.tt", "#{name}/#{name}.gemspec")
     end
     
     def create_config_ru_file
-      template("mafia/templates/config.ru.tt", "#{name}/config.ru")
+      template("config.ru.tt", "#{name}/config.ru")
     end
     
     def create_version_file
-      template("mafia/templates/version.rb.tt", "#{name}/lib/version.rb")
+      template("version.rb.tt", "#{name}/lib/version.rb")
     end
     
     def create_gem_file
-      template("mafia/templates/gem.rb.tt", "#{name}/lib/#{name}.rb")
+      template("gem.rb.tt", "#{name}/lib/#{name}.rb")
     end
     
     def create_app_file
-      template("mafia/templates/app.rb.tt", "#{name}/lib/#{name}/app.rb")
+      template("app.rb.tt", "#{name}/lib/#{name}/app.rb")
     end
     
     def copy_rakefile
-      copy_file("mafia/templates/Rakefile", "#{name}/Rakefile")
+      copy_file("Rakefile", "#{name}/Rakefile")
     end
     
     def copy_gemfile
-      copy_file("mafia/templates/Gemfile", "#{name}/Gemfile")
+      copy_file("Gemfile", "#{name}/Gemfile")
     end
     
     def copy_readme
-      copy_file("mafia/templates/README", "#{name}/README")
+      copy_file("README", "#{name}/README")
     end
     
     def create_empty_files_and_directories
       empty_directory("#{name}/lib/#{name}/public")
       empty_directory("#{name}/lib/#{name}/views")
       empty_directory("#{name}/lib/#{name}/lib")      
+    end
+    
+    def initalize_git_repo
+      target = File.join(Dir.pwd, name)
+      Bundler.ui.info "Initializating git repo in #{target}"
+      Dir.chdir(target) { `git init`; `git add .` }
     end
 
   end # Generator
